3c41197e991d9f4ad55c6bd53ba1b5db3c2684d5
Ayomide Aremu-Cole <121998651+ayomidecole@users.noreply.github.com>
2026-01-14 05:16:25 -0500

    Add to the right side of the hero component

--- Nova Notes/src/components/Hero.jsx
+++ Nova Notes/src/components/Hero.jsx
@@ -16,14 +16,31 @@ const Hero = () => {
                             organize them into projects — even when you’re
                             offline.
                         </p>
-                        <div className='flex items-center space-x-4'>
-                            <button className='bg-blue-600 rounded-full px-4 py-2 text-amber-50'>Start Free</button>
-                            <a href='#' className="text-blue-600 hover:underline">See how it works</a>
+                        <div className="flex items-center space-x-4">
+                            <button className="bg-blue-600 rounded-full px-4 py-2 text-amber-50">
+                                Start Free
+                            </button>
+                            <a
+                                href="#"
+                                className="text-blue-600 hover:underline"
+                            >
+                                See how it works
+                            </a>
                         </div>
                         <small>No credit card required • Cancel anytime</small>
                     </div>
                     {/*Right*/}
-                    <div className="p-8 my-2">hi</div>
+                    <div className="flex flex-col space-y-4 p-8 bg-amber-100 my-2 rounded-lg border-2">
+                        <div className="w-full border rounded-md flex flex-col space-y-4">
+                            <div className="h-6 w-full border-b flex items-center">
+                                <div className="flex items-center space-x-2">
+                                    <div className="w-2 h-2 rounded-full bg-gray-400"></div>
+                                    <div className="w-2 h-2 rounded-full bg-gray-400"></div>
+                                    <div className="w-2 h-2 rounded-full bg-gray-400"></div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
                 </div>
             </div>
         </section>
